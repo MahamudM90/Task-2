0758108cd0fa75600283c08bb9e2358002b076bc
MahamudM90 <mahamud15-10467@diu.edu.bd>
2024-10-04 14:25:43 +0600

    transition use in card

--- fashion-house/src/Components/Hero/Hero.jsx
+++ fashion-house/src/Components/Hero/Hero.jsx
@@ -10,14 +10,14 @@ const Hero = () => {
   return (
     <div className='hero'>
       <animated.section style={fadeIn}>
-        <img src={ecommerce}></img>
+        <img className='w-fit h-fit' src={ecommerce}></img>
         <div className='md:-mt-[550px] md:mx-[500px] max-sm:-mt-[180px] max-sm:mx-[70px]'>
-        <h1 className="fashion-house text-[27px] text-black font-bold">Welcome to you</h1>
+        <h1 className="fashion-house text-[27px] text-black md:font-bold">Welcome to you</h1>
         <br></br>
         <div className='mx-8'>
-        <a href="#cards" className="bg-blue-400 px-6 py-3 rounded-full text-lg hover:bg-blue-200 transition ">Buy Now</a>
+        <a href="#cards" className="bg-blue-400 px-6 py-3 rounded-full text-lg hover:bg-blue-200 transition text-white font-medium">Buy Now</a>
         </div>
-        <h1 className="fashion-house text-[27px] text-black font-bold mt-4">Happy Shopping</h1>
+        <h1 className="fashion-house text-[27px] text-black  mt-4 md:font-bold">Happy Shopping</h1>
         </div>
       </animated.section>
     </div>
